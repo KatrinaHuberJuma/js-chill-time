5666e831a135887c9bec30c6d75dd2e5454f5722
Katrina Huber-Juma <Katrina.huber@gmail.com>
2021-04-01 07:34:16 -0700

    add event listener with alert (as comment to start with, tested in console)

--- static/action.js
+++ static/action.js
@@ -4,3 +4,7 @@ console.log("js is connect");
 
 // $('.fancy-text').addClass("welcome-msg")
 // this selects elements with class fancy-text and add welcome-msg class
+
+// $('#bisque-button').on('click', () => { 
+//     alert("the button was clicked")
+// })
