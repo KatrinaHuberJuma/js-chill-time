2ad2213add51210dbc5c71dd5f16a67258de710a
Katrina Huber-Juma <Katrina.huber@gmail.com>
2021-04-01 07:30:42 -0700

    add comments

--- static/action.js
+++ static/action.js
@@ -2,4 +2,5 @@
 
 console.log("js is connect");
 
-// $('.fancy-text').addClass("welcome-msg") // this selects elements with class fancy-text and add welcome-msg class
+// $('.fancy-text').addClass("welcome-msg")
+// this selects elements with class fancy-text and add welcome-msg class
